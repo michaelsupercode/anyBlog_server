5abce7b86042b777a5051cfd66562b393318ac8e
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-05-28 12:38:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const upload = multer({ dest: './img' })
 app.use(cors({ origin: "*" }))
 app.use('/img', express.static('./img'))
 
-app.get("/", (_, res) => res.send("this server runs quick and fine :)"));
+app.get("/", (_, res) => res.send("server runs quick, stable and fine :)"));
 
 // nodemailer
 const  NODEMAILER_USER = process.env.NODEMAILER_USER
